bbc820927c8ec13d0eee6088980fc0853bb68caa
rails <rails@mylap-top>
2013-12-11 16:02:30 +0530

    new models and services implemented

--- lib/quickbooks/service/term.rb
+++ lib/quickbooks/service/term.rb
@@ -2,21 +2,19 @@ module Quickbooks
   module Service
     class Term < BaseService
       include ServiceCrud
-		
+      
       def delete(term, options = {})
         delete_by_query_string(term)
       end
-
-	private
-        
-	  def default_model_query
+      
+      private
+      def default_model_query
        "SELECT * FROM TERM"
-	  end
-
-	  def model
-	    Quickbooks::Model::Term
-    end
-
+	    end
+      
+      def model
+        Quickbooks::Model::Term
+      end
     end
   end
 end
