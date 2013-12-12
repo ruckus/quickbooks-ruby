efb84b46dfbac511f77352b4687d28b6baa3b328
Cody Caughlan <toolbag@gmail.com>
2013-12-12 09:26:55 -0800

    moved subject settor for realm_id into the test itself to close deprecation warnings from RSpec

--- spec/lib/quickbooks/service/base_service_spec.rb
+++ spec/lib/quickbooks/service/base_service_spec.rb
@@ -1,10 +1,7 @@
 describe Quickbooks::Service::BaseService do
-  before(:all) do
-    subject.realm_id = 1234
-  end
-
   describe "#url_for_query" do
     it "correctly encodes the query" do
+      subject.realm_id = 1234
       query = "SELECT * FROM Customer where Name = 'John'"
 
       correct_url = "https://qb.sbfinance.intuit.com/v3/company/1234/query?query=SELECT+*+FROM+Customer+where+Name+%3D+%27John%27"
