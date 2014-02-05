dc4e637d1a4f1335a65a897d68d711230bae6f64
Joe Wright <joe.wright@noventech.com>
2014-02-05 09:18:10 -0600

    Add time activity service and model.

---
+++ lib/quickbooks/model/time_activity.rb
@@ -0,0 +1,55 @@
+module Quickbooks
+  module Model
+    class TimeActivity < BaseModel
+      XML_COLLECTION_NODE = "TimeActivity"
+      XML_NODE = "TimeActivity"
+      REST_RESOURCE = 'timeactivity'
+
+      NAMEOF_OPTIONS = %w( Employee Vendor )
+
+      xml_name XML_NODE
+
+      xml_accessor :id, :from => 'Id', :as => Integer
+      xml_accessor :sync_token, :from => 'SyncToken', :as => Integer
+      xml_accessor :meta_data, :from => 'MetaData', :as => MetaData
+
+      xml_accessor :txn_date, :from => 'TxnDate'
+      xml_accessor :name_of, :from => 'NameOf'
+
+      xml_accessor :employee_ref, :from => 'EmployeeRef', :as => BaseReference
+      xml_accessor :vendor_ref, :from => 'VendorRef', :as => BaseReference
+      xml_accessor :customer_ref, :from => 'CustomerRef', :as => BaseReference
+      xml_accessor :item_ref, :from => 'ItemRef', :as => BaseReference
+
+      xml_accessor :billable_status, :from => 'BillableStatus'
+      xml_accessor :taxable, :from => 'Taxable'
+      xml_accessor :hours, :from => 'Hours', :as => Integer
+      xml_accessor :hourly_rate, :from => 'HourlyRate'
+      xml_accessor :minutes, :from => 'Minutes', :as => Integer
+      xml_accessor :description, :from => 'Description'
+
+      #== Validations
+      validates_inclusion_of :name_of, :in => NAMEOF_OPTIONS
+      validates_presence_of :employee_ref, :if => Proc.new { |ta| ta.name_of == "Employee" }
+      validates_presence_of :vendor_ref, :if => Proc.new { |ta| ta.name_of == "Vendor" }
+
+      def valid_for_update?
+        if sync_token.nil?
+          errors.add(:sync_token, "Missing required attribute SyncToken for update")
+        end
+        errors.empty?
+      end
+
+      def valid_for_create?
+        valid?
+        errors.empty?
+      end
+
+      def valid_for_deletion?
+        return false if(id.nil? || sync_token.nil?)
+        id.to_i > 0 && !sync_token.to_s.empty? && sync_token.to_i >= 0
+      end
+
+    end
+  end
+end
---
+++ spec/lib/quickbooks/model/time_activity_spec.rb
@@ -0,0 +1,23 @@
+describe "Quickbooks::Model::TimeActivity" do
+
+  it "parse from XML" do
+    xml = fixture("time_activity.xml")
+    time_activity = Quickbooks::Model::TimeActivity.from_xml(xml)
+    time_activity.sync_token.should == 0
+    time_activity.txn_date.should == "2014-02-05"
+    time_activity.name_of.should == "Employee"
+    time_activity.employee_ref.value.should == "66"
+    time_activity.employee_ref.name.should == "John Smith"
+    time_activity.customer_ref.value.should == "123"
+    time_activity.customer_ref.name.should == "Test Customer"
+    time_activity.item_ref.value.should == "49"
+    time_activity.item_ref.name.should == "General Consulting"
+    time_activity.billable_status.should == "Billable"
+    time_activity.taxable.should == "false"
+    time_activity.hourly_rate.should == "115"
+    time_activity.hours.should == 1
+    time_activity.minutes.should == 0
+    time_activity.description.should == "Description 1"
+  end
+
+end
