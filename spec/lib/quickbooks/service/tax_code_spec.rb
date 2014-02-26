7937b6c43be91b07acfec11f8c997fd2296b0329
Jason Dew <jason.dew@gmail.com>
2014-02-26 11:54:03 -0500

    Adding tests for tax rate and code services

---
+++ spec/lib/quickbooks/service/tax_code_spec.rb
@@ -0,0 +1,15 @@
+describe "Quickbooks::Service::TaxCode" do
+  before(:all) { construct_service :tax_code }
+
+  it "can query tax codes" do
+    stub_request(:get,
+                 @service.url_for_query,
+                 ["200", "OK"],
+                 fixture("tax_codes.xml"))
+
+    tax_codes = @service.query
+
+    tax_codes.entries.count.should eq(11)
+    tax_codes.entries.first.name.should eq("TAX")
+  end
+end
---
+++ spec/lib/quickbooks/service/tax_rate_spec.rb
@@ -0,0 +1,15 @@
+describe "Quickbooks::Service::TaxRate" do
+  before(:all) { construct_service :tax_rate }
+
+  it "can query tax rates" do
+    stub_request(:get,
+                 @service.url_for_query,
+                 ["200", "OK"],
+                 fixture("tax_rates.xml"))
+
+    tax_rates = @service.query
+
+    tax_rates.entries.count.should eq(1)
+    tax_rates.entries.first.name.should eq("Mountain View")
+  end
+end
