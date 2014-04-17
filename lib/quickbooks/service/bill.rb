ecdf9e0f086d058e1864924b8a0c0042f7069dbb
Pavel Pachkovskij <pavel.pachkovskij@gmail.com>
2014-04-17 12:59:34 +0300

    add delete to bill service

--- lib/quickbooks/service/bill.rb
+++ lib/quickbooks/service/bill.rb
@@ -2,6 +2,10 @@ module Quickbooks
   module Service
     class Bill < BaseService
 
+      def delete(bill, options = {})
+        delete_by_query_string(bill)
+      end
+
       def default_model_query
         "SELECT * FROM Bill"
       end
