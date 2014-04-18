4b70b5d0662a340bb871aa2cf5a868571457718c
Mark Rickert <mjar81@gmail.com>
2014-04-18 15:04:02 -0400

    Adds Class model and service.

---
+++ lib/quickbooks/service/class.rb
@@ -0,0 +1,27 @@
+module Quickbooks
+  module Service
+    class Class < BaseService
+
+      def update(entity, options = {})
+        raise Quickbooks::InvalidModelException.new('Class sparse update is not supported by Intuit at this time') if options[:sparse] && options[:sparse] == true
+        super(entity, options)
+      end
+
+      def delete(c)
+        c.active = false
+        update(c, :sparse => false)
+      end
+
+      private
+
+      def default_model_query
+        "SELECT * FROM Class"
+      end
+
+      def model
+        Quickbooks::Model::Class
+      end
+
+    end
+  end
+end
