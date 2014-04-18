fc69439df715f34ed9a3f980e3708e804fbfd9cc
Mark Rickert <mjar81@gmail.com>
2014-04-18 13:43:32 -0400

    Add department model and service.

---
+++ lib/quickbooks/model/department.rb
@@ -0,0 +1,21 @@
+module Quickbooks
+  module Model
+    class Department < BaseModel
+      XML_COLLECTION_NODE = "Department"
+      XML_NODE = "Department"
+      REST_RESOURCE = 'Department'
+
+      xml_name XML_NODE
+      xml_accessor :id, :from => 'Id', :as => Integer
+      xml_accessor :sync_token, :from => 'SyncToken', :as => Integer
+      xml_accessor :meta_data, :from => 'MetaData', :as => MetaData
+      xml_accessor :name, :from => 'Name'
+      xml_accessor :sub_department, :from => 'SubDepartment'
+      xml_accessor :parent_ref, :from => 'ParentRef', :as => BaseReference
+      xml_accessor :fully_qualified_name, :from => 'FullyQualifiedName' # ReadOnly
+      xml_accessor :active?, :from => 'Active'
+
+      reference_setters :parent_ref
+    end
+  end
+end
