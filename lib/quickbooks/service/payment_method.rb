31cea6c6c15428fd801759053687a708707a068d
Mark Rickert <mjar81@gmail.com>
2014-04-28 10:14:51 -0400

    Search_name_query shouldn't be private.

--- lib/quickbooks/service/payment_method.rb
+++ lib/quickbooks/service/payment_method.rb
@@ -6,14 +6,14 @@ module Quickbooks
         self.query(search_name_query(name)).entries.first
       end
 
+      def search_name_query(name)
+        "SELECT * FROM PaymentMethod WHERE Name = '#{name}'"
+      end
+
       private
 
       def model
         Quickbooks::Model::PaymentMethod
-      end
-
-      def search_name_query(name)
-        "SELECT * FROM PaymentMethod WHERE Name = '#{name}'"
       end
     end
   end
