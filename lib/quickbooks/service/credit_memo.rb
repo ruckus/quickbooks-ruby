1a885d5804fb54bc6f68b15018c33531a8cb30b3
Mark Rickert <mjar81@gmail.com>
2014-05-20 09:55:04 -0400

    allow full deletion of credit memos.

--- lib/quickbooks/service/credit_memo.rb
+++ lib/quickbooks/service/credit_memo.rb
@@ -2,8 +2,12 @@ module Quickbooks
   module Service
     class CreditMemo < BaseService
 
+      def delete(credit_memo, options = {})
+        delete_by_query_string(credit_memo)
+      end
+
       private
-      
+
       def model
         Quickbooks::Model::CreditMemo
       end
