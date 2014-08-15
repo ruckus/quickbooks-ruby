bb0717649b11fa21074082bebe84052a94d9a598
Arthur Chui <arthur.chui@gmail.com>
2014-08-15 16:18:32 -0700

    QueryBuilder supports DateTime, Time and Date value

--- spec/lib/quickbooks/util/query_builder_spec.rb
+++ spec/lib/quickbooks/util/query_builder_spec.rb
@@ -1,8 +1,21 @@
 describe Quickbooks::Util::QueryBuilder do
   it "can escape a single-quoted query" do
-    util = Quickbooks::Util::QueryBuilder.new
     expected = "DisplayName LIKE '%O\\'Halloran'"
-    generated = util.clause("DisplayName", "LIKE", "%O'Halloran")
+    generated = subject.clause("DisplayName", "LIKE", "%O'Halloran")
+    expected.should == generated
+  end
+
+  it "parses a Time value to the required format" do
+    time = Time.new(2014, 10, 31, 2, 2, 2, "+00:00")
+    expected = "Metadata.LastUpdatedTime > '#{time.iso8601}'"
+    generated = subject.clause("Metadata.LastUpdatedTime", ">", time)
+    expected.should == generated
+  end
+
+  it "parses a Date value the required format" do
+    date = Date.new(2014)
+    expected = "DueDate > '#{date.strftime('%Y-%m-%d')}'"
+    generated = subject.clause("DueDate", ">", date)
     expected.should == generated
   end
 end
