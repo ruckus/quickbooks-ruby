5c44d5a226e8e5ca7a9dcc99edc273db7167f50c
Ben Zittlau <ben@zittlau.ca>
2014-11-02 12:58:46 -0700

    Adding additional support for payment methods.

--- lib/quickbooks/model/payment_method.rb
+++ lib/quickbooks/model/payment_method.rb
@@ -4,11 +4,21 @@ module Quickbooks
       REST_RESOURCE = 'paymentmethod'
       XML_COLLECTION_NODE = "PaymentMethod"
       XML_NODE = "PaymentMethod"
+      include NameEntity::PermitAlterations
+
+      CREDIT_CARD = "CREDIT_CARD"
+      NON_CREDIT_CARD = "NON_CREDIT_CARD"
+
+      PAYMENT_METHOD_TYPES = [CREDIT_CARD, NON_CREDIT_CARD]
 
       xml_accessor :id, :from => 'Id', :as => Integer
+      xml_accessor :sync_token, :from => 'SyncToken', :as => Integer
       xml_accessor :name, :from => 'Name'
+      xml_accessor :type, :from => 'Type'
       xml_accessor :active?, :from => 'Active'
       xml_accessor :amount, :from => 'Amount', :as => BigDecimal, :to_xml => to_xml_big_decimal
+
+      validates_inclusion_of :type, :in => PAYMENT_METHOD_TYPES, :allow_nil => true
     end
   end
 end
