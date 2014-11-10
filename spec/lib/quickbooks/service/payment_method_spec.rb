36f1435d65727995549dbfec31a09424dd175f96
cody caughlan <toolbag@gmail.com>
2014-11-09 16:05:24 -0800

    * Fixed RSpec warning notice about using let/subject in before(:all)

--- spec/lib/quickbooks/service/payment_method_spec.rb
+++ spec/lib/quickbooks/service/payment_method_spec.rb
@@ -4,27 +4,27 @@ module Quickbooks
   module Service
     describe PaymentMethod do
       before(:all) do
-        construct_service :payment_method
-        subject.company_id = "1014843225"
-        subject.access_token = construct_oauth
+        @service = construct_service :payment_method
+        @service.company_id = "1014843225"
+        @service.access_token = construct_oauth
       end
 
       it "queries for payment methods" do
         xml = fixture("payment_methods.xml")
         model = Model::PaymentMethod
-        stub_request(:get, subject.url_for_query, ["200", "OK"], xml)
+        stub_request(:get, @service.url_for_query, ["200", "OK"], xml)
 
-        payment_methods = subject.query.entries
+        payment_methods = @service.query.entries
         expect(payment_methods.count).to eq 8
       end
 
       it "queries a payment method by name" do
         xml = fixture("fetch_payment_method_by_name.xml")
         model = Model::PaymentMethod
-        url = subject.url_for_query(subject.search_name_query("Discover"))
+        url = @service.url_for_query(@service.search_name_query("Discover"))
         stub_request(:get, url, ["200", "OK"], xml)
 
-        payment_method = subject.fetch_by_name("Discover")
+        payment_method = @service.fetch_by_name("Discover")
         expect(payment_method.name).to eq "Discover"
       end
 
