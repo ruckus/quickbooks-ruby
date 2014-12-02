466979e09b4f3e5bb58a97734707499f713fc5b9
Ben Zittlau <ben@zittlau.ca>
2014-12-02 11:58:58 -0700

    Adding a failing test for the journal entry bug.

--- spec/lib/quickbooks/model/journal_entry_spec.rb
+++ spec/lib/quickbooks/model/journal_entry_spec.rb
@@ -9,7 +9,7 @@ describe "Quickbooks::Model::JournalEntry" do
     line_item.description = 'Received Payment'
     line_item.amount      = 148.99
     line_item.detail_type = 'JournalEntryLineDetail'
-    jel = Quickbooks::Model::JournalEntryLineDetail.new  
+    jel = Quickbooks::Model::JournalEntryLineDetail.new
     jel.posting_type = 'Credit'
     jel.tax_code_id = 2
     jel.tax_applicable_on = 'Credit'
@@ -20,5 +20,11 @@ describe "Quickbooks::Model::JournalEntry" do
     n.at_css('JournalEntry > Line > Description').content.should == 'Received Payment'
     n.at_css('Line > JournalEntryLineDetail > TaxCodeRef').content.should == '2'
     je.valid?.should be_true
+  end
+
+  it "parse from XML" do
+    xml = fixture("journal_entry.xml")
+    item = Quickbooks::Model::JournalEntry.from_xml(xml)
+    item.id.should == 450
   end
 end
