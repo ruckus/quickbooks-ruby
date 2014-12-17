5ba13162dbce6fa188f3bcd8e75e97991e9275ee
Oskar Niburski <oskarniburski@gmail.com>
2014-12-17 12:58:57 -0500

    URI encoding for date macro

--- lib/quickbooks/service/reports.rb
+++ lib/quickbooks/service/reports.rb
@@ -8,11 +8,8 @@ module Quickbooks
         Quickbooks::Model::Reports
       end
 
-      def url_for_query(query = nil, start_position = 1, max_results = 20)
-        query ||= default_model_query
-        query = "#{query} STARTPOSITION #{start_position} MAXRESULTS #{max_results}"
-
-        "#{url_for_base}/reports/BalanceSheet?date_macro='This Fiscal Year-to-date'"
+      def url_for_query(date_macro = 'This Fiscal Year-to-date')
+        "#{url_for_base}/reports/BalanceSheet?date_macro=#{URI.encode_www_form_component(date_macro)"
       end
     end
   end
