c27908e0db5fb909cfcaab9487c50b9b0cf4da4f
Oskar Niburski <oskarniburski@gmail.com>
2014-12-17 12:51:13 -0500

    Overridden url for query in reports

--- lib/quickbooks/service/reports.rb
+++ lib/quickbooks/service/reports.rb
@@ -7,6 +7,13 @@ module Quickbooks
       def model
         Quickbooks::Model::Reports
       end
+
+      def url_for_query(query = nil, start_position = 1, max_results = 20)
+        query ||= default_model_query
+        query = "#{query} STARTPOSITION #{start_position} MAXRESULTS #{max_results}"
+
+        "#{url_for_base}/reports/BalanceSheet?date_macro='This Fiscal Year-to-date'"
+      end
     end
   end
 end
