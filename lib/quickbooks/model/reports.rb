041705e59a88ead546f9492fefddb78a7f5822e2
Oskar Niburski <oskarniburski@gmail.com>
2014-12-17 12:20:01 -0500

    Update to actual files

--- lib/quickbooks/model/reports.rb
+++ lib/quickbooks/model/reports.rb
@@ -1,12 +1,17 @@
 module Quickbooks
-  module Service
-    class Reports < BaseService
+  module Model
+    class Reports < BaseModel
+      XML_COLLECTION_NODE = "Report"
+      XML_NODE = "Report"
+      REST_RESOURCE = 'report'
 
-      private
-      
-      def model
-        Quickbooks::Model::Reports
-      end
+      REPORTS = ['BalanceSheet', 'ProfitandLoss', 'ProfitandLossDetail', 'CashFlow']
+
+      xml_name XML_NODE
+
+      xml_accessor :currency, :from => 'Currency'
+
+
     end
   end
 end
