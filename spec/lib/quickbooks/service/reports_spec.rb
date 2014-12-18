64360b72e5463c38644d205794f812a8f63af231
Oskar Niburski <oskarniburski@gmail.com>
2014-12-18 11:46:35 -0500

    CashFlow and Profit Loss added

--- spec/lib/quickbooks/service/reports_spec.rb
+++ spec/lib/quickbooks/service/reports_spec.rb
@@ -4,22 +4,15 @@ describe "Quickbooks::Service::Reports" do
   end
 
   it "can query for reports", focus: true do
-    xml = fixture("reports.xml")
+    xml = fixture("balancesheet.xml")
     model = Quickbooks::Model::Reports
 
     stub_request(:get, @service.url_for_query, ["200", "OK"], xml)
-    reports = @service.query
+    reports = @service.query('BalanceSheet')
 
     balance_sheet_xml = @service.last_response_xml
-    balance_sheet_xml.xpath('//xmlns:Currency').children.to_s == 'USD'
+    balance_sheet_xml.xpath('//xmlns:ReportName').children.to_s == 'BalanceSheet'
   end
-
-  # it 'can grab xml tabular data' do 
-  #   xml = fixture("reports.xml")
-  #   model = Quickbooks::Model::Reports
-
-  #   stub_request(:get, @service.url_for_query, ["200", "OK"], xml)
-  # end
 
   # it 'can query between different dates' do 
   #   xml = fixture("reports.xml")
@@ -28,19 +21,17 @@ describe "Quickbooks::Service::Reports" do
   #   stub_request(:get, @service.url_for_query, ["200", "OK"], xml)
   # end
 
-  # it 'can query balancesheets' do 
-  #   xml = fixture("reports.xml")
-  #   model = Quickbooks::Model::Reports
+  it 'can query cashflow' do 
+    xml = fixture("cashflow.xml")
+    model = Quickbooks::Model::Reports
 
-  #   stub_request(:get, @service.url_for_query, ["200", "OK"], xml)
-  # end
+    stub_request(:get, @service.url_for_query('CashFlow'), ["200", "OK"], xml)
 
-  # it 'can query cashflow' do 
-  #   xml = fixture("reports.xml")
-  #   model = Quickbooks::Model::Reports
+    reports = @service.query('CashFlow')
 
-  #   stub_request(:get, @service.url_for_query, ["200", "OK"], xml)
-  # end
+    cash_flow_xml = @service.last_response_xml
+    cash_flow_xml.xpath('//xmlns:ReportName').children.to_s == 'CashFlow'
+  end
 
   # it 'can query cashflow for different date ranges' do 
   #   xml = fixture("reports.xml")
@@ -49,12 +40,17 @@ describe "Quickbooks::Service::Reports" do
   #   stub_request(:get, @service.url_for_query, ["200", "OK"], xml)
   # end
 
-  # it 'can query profit and loss' do 
-  #   xml = fixture("reports.xml")
-  #   model = Quickbooks::Model::Reports
+  it 'can query profit and loss' do 
+    xml = fixture("profitloss.xml")
+    model = Quickbooks::Model::Reports
 
-  #   stub_request(:get, @service.url_for_query, ["200", "OK"], xml)
-  # end
+    stub_request(:get, @service.url_for_query('ProfitAndLoss'), ["200", "OK"], xml)
+
+    reports = @service.query('ProfitAndLoss')
+
+    profit_loss_xml = @service.last_response_xml
+    profit_loss_xml.xpath('//xmlns:ReportName').children.to_s == 'ProfitAndLoss'
+  end
 
   # it 'can query profit and loss over different date ranges' do 
   #   xml = fixture("reports.xml")
