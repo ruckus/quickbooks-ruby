fe238f15a08d60790805d38ff541dd25f8183e3b
Aaron Kirley <kirley@gmail.com>
2015-02-23 22:15:48 -0500

    invoice send logic

--- lib/quickbooks/service/invoice.rb
+++ lib/quickbooks/service/invoice.rb
@@ -6,8 +6,14 @@ module Quickbooks
         delete_by_query_string(invoice)
       end
 
+      def send(invoice, emailAddress=nil)
+        url = "#{url_for_base}/invoice/#{invoice.id}/send"
+        url = "#{url}?sendTo=emailAddress" if emailAddress.present?
+        response = do_http_post(url, "", {}, {})
+      end
+
       private
-      
+
       def model
         Quickbooks::Model::Invoice
       end
