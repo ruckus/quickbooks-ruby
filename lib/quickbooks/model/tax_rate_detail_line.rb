22dc4530453ab34fbc302fc1fad9c4d98682f43b
Raecoo Cao <raecoo@gmail.com>
2015-07-29 22:33:47 +0800

    fixes length validation for tax_rate_name

--- lib/quickbooks/model/tax_rate_detail_line.rb
+++ lib/quickbooks/model/tax_rate_detail_line.rb
@@ -8,7 +8,7 @@ module Quickbooks
       xml_accessor :tax_agency_id, :from => "TaxAgencyId"
       xml_accessor :tax_applicable_on, :from => 'TaxApplicableOn', :as => :text
 
-      validates :tax_rate_name, presence: true, length: { maximum: 10 }, if: Proc.new {|line| line.tax_rate_id.blank?}
+      validates :tax_rate_name, presence: true, length: { maximum: 100 }, if: Proc.new {|line| line.tax_rate_id.blank?}
       validates :tax_agency_id, presence: true, numericality: {greater_than: 0}, if: Proc.new {|line| line.tax_rate_id.blank?}
       validates :rate_value, presence: true, numericality: {less_than_or_equal_to: 100},  if: Proc.new {|line| line.tax_rate_id.blank?}
       validates :tax_rate_id, numericality: true, if: Proc.new {|line| line.tax_rate_name.blank? && tax_agency_id.blank? && rate_value.blank? }
