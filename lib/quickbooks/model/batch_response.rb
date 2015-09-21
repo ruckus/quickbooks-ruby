fa55cea600c58b4c5a63b6c715dfdfd6954ae961
Arthur Chui <arthur.chui@gmail.com>
2015-09-21 13:39:03 -0700

    support batch processing and change api of RefundReceipt

--- lib/quickbooks/model/batch_response.rb
+++ lib/quickbooks/model/batch_response.rb
@@ -18,6 +18,7 @@ class Quickbooks::Model::BatchResponse < Quickbooks::Model::BaseModel
     xml_accessor :credit_memo, from: "CreditMemo", as: Quickbooks::Model::CreditMemo
     xml_accessor :payment, from: "Payment", as: Quickbooks::Model::Payment
     xml_accessor :time_activity, from: "TimeActivity", as: Quickbooks::Model::TimeActivity
+    xml_accessor :refund_receipt, from: "RefundReceipt", as: Quickbooks::Model::RefundReceipt
 
     def fault?
       fault
