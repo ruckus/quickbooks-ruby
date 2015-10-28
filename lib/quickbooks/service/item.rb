057d4907fe3546111c94dfccc4845e5480036d5d
Don Vanboom <dvboom@hotmail.com>
2015-10-28 14:42:13 -0500

    added minorversion 4 support to Item service and model

--- lib/quickbooks/service/item.rb
+++ lib/quickbooks/service/item.rb
@@ -7,6 +7,16 @@ module Quickbooks
         update(item, :sparse => true)
       end
 
+      def url_for_resource(resource)
+        url = super(resource)
+        url + "?minorversion=4"
+      end
+
+      def url_for_query(query = nil, start_position = 1, max_results = 20)
+        url = super(query, start_position, max_results)
+        url + "&minorversion=4"
+      end
+
       private
 
       def model
