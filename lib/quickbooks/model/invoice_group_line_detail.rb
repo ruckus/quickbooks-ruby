6f6d0de0169aeaa448ac29a553b79d7546fab074
Florin T.PATRASCU <florin.patrascu@gmail.com>
2016-10-15 15:39:02 -0400

    add support for understanding the new Invoice line items of type: GroupLineDetail. These are the item bundles, respectively. See Intuit's docs, for more details: https://developer.intuit.com/docs/0100_quickbooks_online/0200_dev_guides/accounting/item_bundles_using_groups

---
+++ lib/quickbooks/model/invoice_group_line_detail.rb
@@ -0,0 +1,13 @@
+module Quickbooks
+  module Model
+    class InvoiceGroupLineDetail < BaseModel
+      include HasLineItems
+
+      xml_accessor :group_item_ref, :from => 'GroupItemRef', :as => BaseReference
+      xml_accessor :quantity, :from => 'Quantity', :as => BigDecimal, :to_xml => to_xml_big_decimal
+      xml_accessor :line_items, :from => 'Line', :as => [InvoiceLineItem]
+
+      reference_setters :group_item_ref
+    end
+  end
+end
