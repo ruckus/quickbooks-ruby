7103a227f326254e469a6954d5f40dcdef91afba
andrew morton <drewish@katherinehouse.com>
2016-11-20 22:38:12 -0700

    Pass query params (e.g. requestid) with batch
    Fixes #343

--- lib/quickbooks/service/batch.rb
+++ lib/quickbooks/service/batch.rb
@@ -3,7 +3,7 @@ module Quickbooks
     class Batch < BaseService
 
       def make_request(entity, options = {})
-        response = do_http_post(url_for_resource('batch'), valid_xml_document(entity.to_xml_ns))
+        response = do_http_post(url_for_resource('batch'), valid_xml_document(entity.to_xml_ns), options[:query])
         Quickbooks::Model::BatchResponse.from_xml(response.plain_body)
       end
     end
