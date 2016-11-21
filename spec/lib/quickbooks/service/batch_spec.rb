9e5041ff806582e2ac11d90c42e89d3064a977b3
andrew morton <drewish@katherinehouse.com>
2016-11-20 22:38:12 -0700

    Pass query params (e.g. requestid) with batch
    Fixes #343

--- spec/lib/quickbooks/service/batch_spec.rb
+++ spec/lib/quickbooks/service/batch_spec.rb
@@ -17,4 +17,12 @@ describe "Quickbooks::Service::Batch" do
       @service.make_request(Quickbooks::Model::BatchRequest.new)
     }.should raise_error(Quickbooks::IntuitRequestException)
   end
+
+  it "allows user to specify a RequestId in a create call" do
+    requestid = "foobar123"
+    xml = fixture("batch_response.xml")
+    stub_request(:post, "#{@service.url_for_resource('batch')}?requestid=#{requestid}", ["200", "OK"], xml)
+    batch_resp = @service.make_request(Quickbooks::Model::BatchRequest.new, :query => {:requestid => requestid})
+    batch_resp.class.should == Quickbooks::Model::BatchResponse
+  end
 end
