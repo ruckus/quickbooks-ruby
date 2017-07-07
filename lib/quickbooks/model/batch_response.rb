e9f73da032e5a453ec52fc6c45b5985eecc64d66
Jimmy Baker <jimmyebaker@gmail.com>
2017-07-07 09:25:32 -0500

    Added purchase order to the list of batch request/response entities. Sorted entities alphabetically.

--- lib/quickbooks/model/batch_response.rb
+++ lib/quickbooks/model/batch_response.rb
@@ -7,19 +7,20 @@ class Quickbooks::Model::BatchResponse < Quickbooks::Model::BaseModel
     xml_accessor :fault, :from => 'Fault', :as => Quickbooks::Model::Fault
 
     # Supported Entities
-    xml_accessor :item, from: "Item", as: Quickbooks::Model::Item
     xml_accessor :account, from: "Account", as: Quickbooks::Model::Account
-    xml_accessor :invoice, from: "Invoice", as: Quickbooks::Model::Invoice
-    xml_accessor :customer, from: "Customer", as: Quickbooks::Model::Customer
-    xml_accessor :bill, from: "Bill", as: Quickbooks::Model::Bill
-    xml_accessor :sales_receipt, from: "SalesReceipt", as: Quickbooks::Model::SalesReceipt
     xml_accessor :bill_payment, from: "BillPayment", as: Quickbooks::Model::BillPayment
-    xml_accessor :purchase, from: "Purchase", as: Quickbooks::Model::Purchase
+    xml_accessor :bill, from: "Bill", as: Quickbooks::Model::Bill
     xml_accessor :credit_memo, from: "CreditMemo", as: Quickbooks::Model::CreditMemo
-    xml_accessor :payment, from: "Payment", as: Quickbooks::Model::Payment
-    xml_accessor :time_activity, from: "TimeActivity", as: Quickbooks::Model::TimeActivity
-    xml_accessor :refund_receipt, from: "RefundReceipt", as: Quickbooks::Model::RefundReceipt
+    xml_accessor :customer, from: "Customer", as: Quickbooks::Model::Customer
+    xml_accessor :invoice, from: "Invoice", as: Quickbooks::Model::Invoice
+    xml_accessor :item, from: "Item", as: Quickbooks::Model::Item
     xml_accessor :journal_entry, from: "JournalEntry", as: Quickbooks::Model::JournalEntry
+    xml_accessor :payment, from: "Payment", as: Quickbooks::Model::Payment
+    xml_accessor :purchase_order, from: "PurchaseOrder", as: Quickbooks::Model::PurchaseOrder
+    xml_accessor :purchase, from: "Purchase", as: Quickbooks::Model::Purchase
+    xml_accessor :refund_receipt, from: "RefundReceipt", as: Quickbooks::Model::RefundReceipt
+    xml_accessor :sales_receipt, from: "SalesReceipt", as: Quickbooks::Model::SalesReceipt
+    xml_accessor :time_activity, from: "TimeActivity", as: Quickbooks::Model::TimeActivity
     xml_accessor :vendor, from: "Vendor", as: Quickbooks::Model::Vendor
 
     def fault?
