49cbb81aa347821323c58f8f3e103c37e11485d3
cody caughlan <toolbag@gmail.com>
2018-03-14 19:36:53 -0700

    Create operations use HTTP POST and those response bodies are Gzip-encoded. The Faraday HTTP client adapter used by the oauth2 gem will transparently decompress HTTP#get responses. But we need to also decompress any HTTP#post responses. Without this decompression the response checking logic bugs out.
    I borrowed the Faraday gzip middleware from: https://github.com/lostisland/faraday_middleware/blob/master/lib/faraday_middleware/gzip.rb  thank you!
    
    Fixes #421

--- lib/quickbooks-ruby.rb
+++ lib/quickbooks-ruby.rb
@@ -14,6 +14,7 @@ require 'quickbooks/util/logging'
 require 'quickbooks/util/http_encoding_helper'
 require 'quickbooks/util/name_entity'
 require 'quickbooks/util/query_builder'
+require 'quickbooks/faraday/middleware/gzip'
 
 #== OAuth Responses
 require 'quickbooks/service/responses/oauth_http_response'
@@ -180,6 +181,9 @@ require 'quickbooks/service/payment_change'
 require 'quickbooks/service/transfer'
 require 'quickbooks/service/change_data_capture'
 require 'quickbooks/service/refund_receipt_change'
+
+# Register Faraday Middleware
+Faraday::Middleware.register_middleware :gzip => lambda { Gzip }
 
 module Quickbooks
   @@sandbox_mode = false
