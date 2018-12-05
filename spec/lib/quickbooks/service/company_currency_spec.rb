a7c0646c2d40aecdc53962e57affc7a2e6f6d543
Chris Green <chrisgreen1993@gmail.com>
2018-12-05 22:05:15 +0000

    Add delete method to service

--- spec/lib/quickbooks/service/company_currency_spec.rb
+++ spec/lib/quickbooks/service/company_currency_spec.rb
@@ -12,4 +12,15 @@ describe "Quickbooks::Service::CompanyCurrency" do
     company_currency = company_currency_query.entries.first
     company_currency.name.should == 'Euro'
   end
+
+  it 'can delete a company_currency' do
+    xml = fixture('deleted_company_currency.xml')
+    model = Quickbooks::Model::CompanyCurrency
+    company_currency = model.new(:id => 1, :sync_token => 2, :code => 'EUR')
+    stub_request(:post, @service.url_for_resource(model::REST_RESOURCE), ["200", "OK"], xml, {}, true)
+
+    company_currency.should be_valid_for_deletion
+    deleted_company_currency = @service.delete(company_currency)
+    deleted_company_currency.should_not be_active
+  end
 end
