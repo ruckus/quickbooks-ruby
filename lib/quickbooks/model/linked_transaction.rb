4a1a104e46cee110191178c6eab5051a13ca3411
vkomarov <vitalij@urbandoor.com>
2019-01-07 11:11:58 -0800

    adding LinkedTxn collection into Quickbooks::Model::Bill per v3 API spec

--- lib/quickbooks/model/linked_transaction.rb
+++ lib/quickbooks/model/linked_transaction.rb
@@ -1,9 +1,16 @@
 module Quickbooks
   module Model
     class LinkedTransaction < BaseModel
+      #== Constants
+      DEFAULT_TXN_TYPE = 'BillPaymentCheck'.freeze
+
       xml_accessor :txn_id, :from => 'TxnId'
       xml_accessor :txn_type, :from => 'TxnType'
       xml_accessor :txn_line_id, :from => 'TxnLineId'
+
+      def bill_payment_check_type?
+        txn_type.to_s == DEFAULT_TXN_TYPE
+      end
     end
   end
 end
