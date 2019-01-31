fc3edff35baf588a8729a0978be832e9ca4931ee
cody caughlan <toolbag@gmail.com>
2019-01-30 20:10:51 -0800

    * Implemented minorversion support for Customer (at 33) * Added PrefVendorRef field support to Item

--- lib/quickbooks/service/customer.rb
+++ lib/quickbooks/service/customer.rb
@@ -7,6 +7,21 @@ module Quickbooks
         update(customer, :sparse => true)
       end
 
+      def url_for_resource(resource)
+        url = super(resource)
+        "#{url}?minorversion=#{Quickbooks::Model::Customer::MINORVERSION}"
+      end
+
+      def fetch_by_id(id, params = {})
+        url = "#{url_for_base}/customer/#{id}?minorversion=#{Quickbooks::Model::Customer::MINORVERSION}"
+        fetch_object(model, url, params)
+      end
+
+      def url_for_query(query = nil, start_position = 1, max_results = 20, options = {})
+        url = super(query, start_position, max_results, options)
+        "#{url}&minorversion=#{Quickbooks::Model::Customer::MINORVERSION}"
+      end
+
       private
 
       def model
