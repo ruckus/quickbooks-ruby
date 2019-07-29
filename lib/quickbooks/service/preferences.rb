8348a5bc9bd5aafb91c3827f2d885177be26fefa
Andrew Porterfield <ajporterfield@gmail.com>
2019-07-28 20:43:17 -0400

    Adds support for accessing BookCloseDate from the Preferences service/model

--- lib/quickbooks/service/preferences.rb
+++ lib/quickbooks/service/preferences.rb
@@ -2,8 +2,13 @@ module Quickbooks
   module Service
     class Preferences < BaseService
 
+      def url_for_query(query = nil, start_position = 1, max_results = 20, options = {})
+        url = super(query, start_position, max_results, options)
+        "#{url}&minorversion=#{Quickbooks::Model::Preferences::MINORVERSION}"
+      end
+
       private
-      
+
       def model
         Quickbooks::Model::Preferences
       end
