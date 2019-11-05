f31ed1bca9af519a000726e067172f02527c5b58
Colm Heaney <cheaney@shopkeep.com>
2019-11-05 11:18:07 +0000

    Add minorversion to account service and update query url

--- lib/quickbooks/service/account.rb
+++ lib/quickbooks/service/account.rb
@@ -7,6 +7,11 @@ module Quickbooks
         update(account, :sparse => true)
       end
 
+      def url_for_query(query = nil, start_position = 1, max_results = 20, options = {})
+        url = super(query, start_position, max_results, options)
+        "#{url}&minorversion=#{Quickbooks::Model::Account::MINORVERSION}"
+      end
+
       private
 
       def model
