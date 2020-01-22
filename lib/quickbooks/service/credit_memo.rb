7eac7a2a1cb7589f2e1c12cdde1d5acd90446c40
Frank Groeneveld <frenkel@users.noreply.github.com>
2020-01-22 17:18:37 +0100

    Add ability to download credit memo as pdf (#500)

--- lib/quickbooks/service/credit_memo.rb
+++ lib/quickbooks/service/credit_memo.rb
@@ -6,6 +6,12 @@ module Quickbooks
         delete_by_query_string(credit_memo)
       end
 
+      def pdf(credit_memo)
+        url = "#{url_for_resource(model::REST_RESOURCE)}/#{credit_memo.id}/pdf"
+        response = do_http_raw_get(url, {}, {'Accept' => 'application/pdf'})
+        response.plain_body
+      end
+
       private
 
       def model
