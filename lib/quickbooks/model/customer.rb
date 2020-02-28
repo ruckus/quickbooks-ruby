4ae9ab0cf60cde5439b1f346410d8ebde2848db7
Greg Kane <gbkane@gmail.com>
2020-02-28 17:47:17 -0500

    Add customer type

--- lib/quickbooks/model/customer.rb
+++ lib/quickbooks/model/customer.rb
@@ -57,12 +57,14 @@ module Quickbooks
       xml_accessor :currency_ref, :from => 'CurrencyRef', :as => BaseReference
       xml_accessor :tax_exemption_reason_id, :from => 'TaxExemptionReasonId'
       xml_accessor :primary_tax_identifier, :from => 'PrimaryTaxIdentifier'
+      xml_accessor :customer_type_ref, :from => 'CustomerTypeRef', :as => BaseReference
 
       #== Validations
       validate :names_cannot_contain_invalid_characters
       validate :email_address_is_valid
 
-      reference_setters :parent_ref, :sales_term_ref, :payment_method_ref, :default_tax_code_ref, :currency_ref
+      reference_setters :parent_ref, :sales_term_ref, :payment_method_ref, :default_tax_code_ref, :currency_ref,
+                        :customer_type_ref
 
       def job?
         job.to_s == 'true'
