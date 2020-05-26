54f4c6933b04f24efa1f916b70fc3a57a858f0f3
jethrodaniel <jethrodaniel@gmail.com>
2020-05-26 17:18:20 -0500

    ensure we `#to_s` any non-date, non-array query values (#521)
    Before this was applied, the following code would blow up with a
    `NoMethodError`.
    
    ```
    util = Quickbooks::Util::QueryBuilder.new
    clause = util.clause('Id', '=', 4)
    ```
    
    Co-authored-by: Cody Caughlan <toolbag@gmail.com>

--- lib/quickbooks/util/query_builder.rb
+++ lib/quickbooks/util/query_builder.rb
@@ -10,26 +10,27 @@ module Quickbooks
       end
 
       def clause(field, operator, value)
+        # replace with an escaped backslash
+        escape_single_quotes = -> field { field.to_s.gsub("'", "\\\\'") }
+
         value = case value
                 when DateTime, Time
                   value.iso8601
                 when Date
                   value.strftime('%Y-%m-%d')
                 when Array
-                  value = value.map{|v| v.to_s.gsub("'", "\\\\'") }
+                  value = value.map(&escape_single_quotes)
                 else
-                  # escape single quotes with an escaped backslash
-                  value = value.to_s.gsub("'", "\\\\'")
+                  value = escape_single_quotes.call(value)
                 end
 
         if operator.downcase == 'in' && value.is_a?(Array)
-          value = value.map{|v| "#{VALUE_QUOTE}#{v}#{VALUE_QUOTE}"}
+          value = value.map { |v| "#{VALUE_QUOTE}#{v}#{VALUE_QUOTE}" }
           "#{field} #{operator} (#{value.join(', ')})"
         else
           "#{field} #{operator} #{VALUE_QUOTE}#{value}#{VALUE_QUOTE}"
         end
       end
-
     end
   end
 end
