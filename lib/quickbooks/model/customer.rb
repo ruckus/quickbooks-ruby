7ed8fba6cec0763218ee395beb2be5aaf9208434
nishantdania <nishantdania@gmail.com>
2021-02-16 08:43:34 +0800

    Added Source to Customer

--- lib/quickbooks/model/customer.rb
+++ lib/quickbooks/model/customer.rb
@@ -56,6 +56,7 @@ module Quickbooks
       xml_accessor :tax_exemption_reason_id, :from => 'TaxExemptionReasonId'
       xml_accessor :primary_tax_identifier, :from => 'PrimaryTaxIdentifier'
       xml_accessor :customer_type_ref, :from => 'CustomerTypeRef', :as => BaseReference
+      xml_accessor :source, :from => 'Source'
 
       #== Validations
       validate :names_cannot_contain_invalid_characters
