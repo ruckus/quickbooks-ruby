2acdfe7f33702367e484060f90f040bc2440d805
Daniel Ho <daniel.ho@housecallpro.com>
2021-11-29 16:49:07 -0800

    Add total_tax_specified xml_accessor to TransactionTaxDetail
    In a recent update to the QBO API, if a company that uses automated sales tax has an invoice that has no tax amount, we need to specify TotalTaxSpecified = false. Not passing this results in an error from QBO indicating that sales tax was miscalculated.

--- lib/quickbooks/model/transaction_tax_detail.rb
+++ lib/quickbooks/model/transaction_tax_detail.rb
@@ -1,9 +1,9 @@
 module Quickbooks
   module Model
     class TransactionTaxDetail < BaseModel
-
       xml_accessor :txn_tax_code_ref, :from => 'TxnTaxCodeRef', :as => BaseReference
       xml_accessor :total_tax, :from => 'TotalTax', :as => BigDecimal, :to_xml => to_xml_big_decimal
+      xml_accessor :total_tax_specified, :from => 'TotalTaxSpecified'
       xml_accessor :lines, :from => 'TaxLine', :as => [TaxLine]
 
       reference_setters :txn_tax_code_ref
--- spec/lib/quickbooks/model/transaction_tax_detail_spec.rb
+++ spec/lib/quickbooks/model/transaction_tax_detail_spec.rb
@@ -1,19 +1,35 @@
-describe "Quickbooks::Model::TransactionTaxDetail" do
-  it "allows setting of the TxnTaxCodeRef via #txn_tax_code_ref_id=" do
-    transaction_tax_detail = Quickbooks::Model::TransactionTaxDetail.new
-    transaction_tax_detail.total_tax = 42
-    transaction_tax_detail.txn_tax_code_id = 42
-    expect(transaction_tax_detail.txn_tax_code_ref.value).to eq(42)
+describe Quickbooks::Model::TransactionTaxDetail do
+  subject(:transaction_tax_detail) { Quickbooks::Model::TransactionTaxDetail.new }
+
+  describe 'txn_tax_code_ref_id' do
+    it "allows setting of the TxnTaxCodeRef via #txn_tax_code_ref_id=" do
+      transaction_tax_detail.total_tax = 42
+      transaction_tax_detail.txn_tax_code_id = 42
+      expect(transaction_tax_detail.txn_tax_code_ref.value).to eq(42)
+    end
   end
 
-  it "total tax should be a decimal/float" do
-    transaction_tax_detail = Quickbooks::Model::TransactionTaxDetail.new
-    transaction_tax_detail.total_tax = 42
-    expect(transaction_tax_detail.to_xml.at_css('TotalTax').content).to eq('42.0')
+  describe 'total tax specified' do
+    context 'when it is set' do
+      before { transaction_tax_detail.total_tax_specified = false; binding.pry }
+
+      it { expect(transaction_tax_detail.to_xml.at_css('TotalTaxSpecified').content).to match('false') }
+    end
+
+    context 'when it is not set' do
+      it { expect(transaction_tax_detail.to_xml).not_to match(/TotalTaxSpecified/) }
+    end
   end
 
-  it "total tax should not be included if not set" do
-    transaction_tax_detail = Quickbooks::Model::TransactionTaxDetail.new
-    expect(transaction_tax_detail.to_xml).not_to match(/TotalTax/)
+  describe 'total tax' do
+    context 'when it is set' do
+      before { transaction_tax_detail.total_tax = 42 }
+
+      it { expect(transaction_tax_detail.to_xml.at_css('TotalTax').content).to eq('42.0') }
+    end
+
+    context 'when it is not set' do
+      it { expect(transaction_tax_detail.to_xml).not_to match(/TotalTax/) }
+    end
   end
 end
